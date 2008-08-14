5fd426906618f41a991f666b78cfc230d65284c2
Scott Doane <scotty@randomproductions.net>
2008-08-14 17:06:49 -0400

    security

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  # before_filter :has_permission?, :except => [ :error, :denied ]
+  
   # GET /pages
   # GET /pages.xml
   def index
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,4 @@
 class Page < ActiveRecord::Base
+  extend AclExtensions
+  
 end
