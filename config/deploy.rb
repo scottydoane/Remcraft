de22115d723fbb7bccb676a0bf3741c33d3f0804
Scott Doane <scotty@randomproductions.net>
2008-08-14 17:24:37 -0400

    database

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,19 @@
 set :application, "remcraft"
 set :repository,  "set your repository location here"
-set :repository,  "http://svn.randomproductions.net/cornerstone/branches/legacy"
+set :repository,  "git@scorpio.randomproductions.net:remcraft.git"
+
+set :user, "randompr"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/randompr/rails/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 set :scm, :git
-set :deploy_to, "/home/randompr/rails/#{application}"
 
 role :app, "scorpio.randomproductions.net"
 role :web, "scorpio.randomproductions.net"
