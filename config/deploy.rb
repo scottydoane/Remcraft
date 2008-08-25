da3f48cae57375de1db6c5293027666f73729fb0
Scott Doane <scotty@geekbehindthecurtain.com>
2008-08-24 21:57:54 -0400

    Staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,32 @@
 set :application, "remcraft"
-set :repository,  "git@scorpio.randomproductions.net:remcraft.git"
+set :repository, "ssh://root@sagittarius.geekbehindthecurtain.com/home/git/repositories/remcraft.git"
+default_run_options[:pty] = true
 
-set :user, "randompr"
+set :user, "root" 
+set :runner, user
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
-set :deploy_to, "/home/randompr/rails/#{application}"
+set :deploy_to, "/var/www/rails/#{application}"
+set :rails_env, "staging"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 set :scm, :git
-set :scm_user, 'randompr'
+set :scm_user, 'scotty'
 
-role :app, "scorpio.randomproductions.net"
-role :web, "scorpio.randomproductions.net"
-role :db,  "scorpio.randomproductions.net", :primary => true
+set :deploy_via, :remote_cache
+set :port, 7822
+
+role :app, "sagittarius.geekbehindthecurtain.com"
+role :web, "sagittarius.geekbehindthecurtain.com"
+role :db,  "sagittarius.geekbehindthecurtain.com", :primary => true
 
 namespace :deploy do  
   task :restart do
-    sudo 'killall lighttpd; sudo lighttpd -f /etc/lighttpd/lighttpd.conf'
+    run '/etc/init.d/lighttpd restart'
   end
 end
