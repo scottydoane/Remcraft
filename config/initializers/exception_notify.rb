da39dbd044d182f3004426febce5d80101f547e4
Scott Doane <scotty@geekbehindthecurtain.com>
2008-10-05 14:48:04 -0400

    fix for exception notify and image sizes

--- config/initializers/exception_notify.rb
+++ config/initializers/exception_notify.rb
@@ -1,2 +1,2 @@
-ExceptionNotifier.exception_recipients = %w(exceptions@geekbehindthecurtain)
+ExceptionNotifier.exception_recipients = %w(exceptions@geekbehindthecurtain.com)
 ExceptionNotifier.sender_address = %("Remcraft Error" <errors@remcraft.com>)
