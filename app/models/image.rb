54a76dcb970977df17d2a1d777a26bcd89b9126a
Scott Doane <scotty@geekbehindthecurtain.com>
2008-10-05 14:25:02 -0400

    image manipulation

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   has_attachment :max_size => 15.megabytes,
     :storage => :file_system,
     :content_type => :image,
-    :processor => 'Rmagick',
+    :processor => 'ImageScience',
     :path_prefix => 'public/images/uploads',
     :thumbnails => {
       :extralarge => '1200>',
