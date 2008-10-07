0fa40a398c239d9e5ec685ba68a98d59980bf49d
Scott Doane <scotty@geekbehindthecurtain.com>
2008-10-06 20:08:51 -0400

    contact us form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,10 @@
 class ContactsController < ApplicationController
   layout 'public'
+  
+  
+  def send_mail
+    MailMan.deliver_comment(params[:email])
+    redirect_to :controller => "info", :action => "thanks"
+  end
+  
 end
--- app/models/mail_man.rb
+++ app/models/mail_man.rb
@@ -1,4 +1,38 @@
 class MailMan < ActionMailer::Base
   
+  # def generic_mailer(options)
+  #   @recipients = options[:recipients] || "me@privacy.net"
+  #   @from       = options[:from]       || "me@privacy.net"
+  #   @cc         = options[:cc]         || ""
+  #   @bcc        = options[:bcc]        || ""
+  #   @subject    = options[:subject]    || ""
+  #   @body       = options[:body]       || {}
+  #   @headers    = options[:headers]    || {}
+  #   @charset    = options[:charset]    || "utf-8"
+  #   @attachment = options[:attachment] || {}
+  # end
+  # 
+  # def MailMan.mails *syms
+  #   syms.each do |sym|
+  #     s = %Q{
+  #       def #{sym}(options) 
+  #         self.generic_mailer(options)
+  #       end
+  #     }
+  #     class_eval s
+  #   end
+  # end
+  # mails :comment
+  
+  def comment(email_params, sent_at = Time.now)
+      @subject = "[Remcraft Website Contact] #{email_params[:subject]}"
+      @recipients  = "scotty@geekbehindthecurtain.com, brian@stripecreative.com"
+      @from  = email_params[:address]
+      @sent_on = sent_at
 
+      # allows access to @message and @sender_name
+      # in the view
+      @body = { :message => email_params[:body], :sender_name => email_params[:name] }
+    end
+    
 end
