8def1eb7e0c94f6ebde7a89524a0c56c746edfe0
Scott Doane <scotty@geekbehindthecurtain.com>
2008-10-09 14:46:10 -0400

    fixes from brian

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   # has_slug :source_column => :name, :prepend_id => false, :sync_slug => true
-  acts_as_category :foreign_key => 'parent_id'
+  acts_as_category :foreign_key => 'parent_id', :order => :position
   
   has_many :images, :as => :imageable, :dependent => :destroy, :order => :position
   
