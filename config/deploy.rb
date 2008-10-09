e1fb12d4e47dc2a476b761f9f528a09491427b59
Scott Doane <scotty@geekbehindthecurtain.com>
2008-10-09 16:47:32 -0400

    changes for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,17 @@ set :application, "remcraft"
 set :repository, "ssh://root@sagittarius.geekbehindthecurtain.com/home/git/repositories/remcraft.git"
 default_run_options[:pty] = true
 
-set :user, "root" 
+set :user, "remcraft" 
 set :runner, user
+set :password, "9b2d6v1s"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
-set :deploy_to, "/var/www/rails/#{application}"
-set :rails_env, "staging"
+set :deploy_to, "/home/remcraft/rails"
+set :rails_env, "production"
+set :use_sudo, false
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -21,19 +23,21 @@ set :git_enable_submodules, true
 
 set :port, 7822
 
-role :app, "sagittarius.geekbehindthecurtain.com"
-role :web, "sagittarius.geekbehindthecurtain.com"
-role :db,  "sagittarius.geekbehindthecurtain.com", :primary => true
+role :app, "www.remcraft.com"
+role :web, "www.remcraft.com"
+role :db,  "www.remcraft.com", :primary => true
 
 namespace :deploy do  
-  task :restart do
-    run '/etc/init.d/lighttpd restart'
-  end
+  # task :restart do
+  #   run '/etc/init.d/lighttpd restart'
+  # end
   
   task :after_symlink do
-    sudo 'sudo chown -R www-data:www-data /var/www/rails/remcraft/releases'
-    sudo 'sudo chmod 777 /var/www/rails/remcraft/current/tmp'
-    sudo 'sudo ln -s /var/www/rails/remcraft/uploads /var/www/rails/remcraft/current/public/images/uploads'
-    sudo 'sudo ln -s /var/www/rails/remcraft/assets /var/www/rails/remcraft/current/public/assets'
+    # sudo 'sudo chown -R www-data:www-data /var/www/rails/remcraft/releases'
+    # sudo 'sudo chmod 777 /var/www/rails/remcraft/current/tmp'
+    # sudo 'sudo ln -s /var/www/rails/remcraft/uploads /var/www/rails/remcraft/current/public/images/uploads'
+    # sudo 'sudo ln -s /var/www/rails/remcraft/assets /var/www/rails/remcraft/current/public/assets'
+    run "ln -s #{deploy_to}/uploads #{deploy_to}/current/public/images/uploads"
+    run "ln -s #{deploy_to}/assets #{deploy_to}/current/public/assets"
   end
 end
