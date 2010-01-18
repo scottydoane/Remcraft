a4bdb32e0b9b8e2d9c06b859064079fbb2270e6e
Scott Doane <scotty@randomproductions.net>
2010-01-18 17:32:33 -0500

    preparing for production deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,22 +3,22 @@ set :repository, "git://github.com/scottydoane/Remcraft.git"
 default_run_options[:pty] = true
 
 # PRODUCTION VALUES
-# set :user, "remcraft" 
-# set :runner, user
-# set :password, "9b2d6v1s"
+set :user, "remcraft" 
+set :runner, user
+set :password, "tigger"
 
-set :user, "root"
-set :password, "pr0d1G10u$"
+#set :user, "root"
+#set :password, "pr0d1G10u$"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
 
-# set :deploy_to, "/home/remcraft/rails"
-set :deploy_to, "/var/www/rails/#{application}"
+set :deploy_to, "/home/remcraft/etc/rails_apps/remcraft_2010"
+# set :deploy_to, "/var/www/rails/#{application}"
 
-set :rails_env, "staging"
+set :rails_env, "production"
 # set :use_sudo, false
 
 # If you aren't using Subversion to manage your source code, specify
@@ -28,16 +28,16 @@ set :scm, :git
 set :scm_user, 'scotty'
 set :git_enable_submodules, true
 
-set :port, 7822
+# set :port, 7822
 
-role :app, "sagittarius.geekbehindthecurtain.com"
-role :web, "sagittarius.geekbehindthecurtain.com"
-role :db,  "sagittarius.geekbehindthecurtain.com", :primary => true
+role :app, "www.remcraft.com"
+role :web, "www.remcraft.com"
+role :db,  "www.remcraft.com", :primary => true
 
 namespace :deploy do  
-  task :restart do
-    run "/etc/init.d/lighttpd restart </dev/null >/dev/null 2>&1 " 
-  end
+  # task :restart do
+  #  run "/etc/init.d/lighttpd restart </dev/null >/dev/null 2>&1 " 
+  # end
   
   task :after_symlink do
     sudo 'sudo chown -R www-data:www-data /var/www/rails/remcraft/releases'
