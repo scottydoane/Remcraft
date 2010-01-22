7054e1a423b38969f477209697da9d8f85b7a43a
Scott Doane <scotty@randomproductions.net>
2010-01-22 17:52:23 -0500

    new deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,36 +3,32 @@ set :repository, "git://github.com/scottydoane/Remcraft.git"
 default_run_options[:pty] = true
 
 # PRODUCTION VALUES
-set :user, "remcraft" 
+set :user, "scotty" 
 set :runner, user
-set :password, "9b2d6v1s"
-
-#set :user, "root"
-#set :password, "pr0d1G10u$"
+set :password, "pr0d1G10u$"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
 
-set :deploy_to, "/home/remcraft/etc/rails_apps/Remcraft2010"
-# set :deploy_to, "/var/www/rails/#{application}"
+set :deploy_to, "/var/www/rails/#{application}.com"
 
 set :rails_env, "production"
-set :use_sudo, false
+set :use_sudo, true
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 set :scm, :git
 set :scm_user, 'scotty'
-# set :git_enable_submodules, true
+set :git_enable_submodules, true
 
 set :port, 7822
 
-role :app, "www.remcraft.com"
-role :web, "www.remcraft.com"
-role :db,  "www.remcraft.com", :primary => true
+role :app, "66.103.254.200"
+role :web, "66.103.254.200"
+role :db,  "66.103.254.200", :primary => true
 
 namespace :deploy do  
   # task :restart do
@@ -44,10 +40,10 @@ namespace :deploy do
     # sudo 'sudo chmod 777 /var/www/rails/remcraft/current/tmp'
     # sudo 'sudo ln -s /var/www/rails/remcraft/uploads /var/www/rails/remcraft/current/public/images/uploads'
     # sudo 'sudo ln -s /var/www/rails/remcraft/assets /var/www/rails/remcraft/current/public/assets'
-    run "rm -rf #{deploy_to}/current/vendor/plugins"
-    run "ln -s #{deploy_to}/plugins #{deploy_to}/current/vendor/plugins"
-    run "ln -s #{deploy_to}/uploads #{deploy_to}/current/public/images/uploads"
-    run "ln -s #{deploy_to}/assets #{deploy_to}/current/public/assets"
+    # run "rm -rf #{deploy_to}/current/vendor/plugins"
+    # run "ln -s #{deploy_to}/plugins #{deploy_to}/current/vendor/plugins"
+    # run "ln -s #{deploy_to}/uploads #{deploy_to}/current/public/images/uploads"
+    # run "ln -s #{deploy_to}/assets #{deploy_to}/current/public/assets"
     # run "ln -s #{deploy_to}/shared/images #{deploy_to}/current/public/images/static"
   end
   
